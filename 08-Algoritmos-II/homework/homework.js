42b645bdbbd7b0059056db4de653f23526ae0832
Domenico <dpaganoh@gmail.com>
2023-07-26 22:42:35 -0500

    Homework 08 - Algoritmos II

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,18 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length <= 1) return array;
+    let pivot = array[0];
+    let left = [];
+    let right = [];
+    for (let i=1; i < array.length; i++){
+        if(array[i] <= pivot){
+            left.push(array[i])
+        } else {
+            right.push(array[i])
+        }
+    }
+    return [...quickSort(left),pivot,...quickSort(right)];
 }
 
 function mergeSort(array) {
@@ -14,8 +25,45 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+    if(array.length <= 1) return array;
 
+    let mid = Math.floor(array.length / 2);
+    let left = array.slice(0,mid);
+    let right = array.slice(mid);
+    
+    let sortedL = mergeSort(left);
+    let sortedR = mergeSort(right);
+    
+    return merge(sortedL,sortedR)
   //okey probemos ahora
+}
+
+function merge(left,right){
+  let arrOrdenado = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length){
+      if (left[leftIndex] < right[rightIndex]){
+          arrOrdenado.push(left[leftIndex]);
+          leftIndex++
+      } else {
+          arrOrdenado.push(right[rightIndex]);
+          rightIndex++
+      }
+  }
+  
+  while (leftIndex < left.length){
+      arrOrdenado.push(left[leftIndex]);
+      leftIndex++
+  }
+  
+  while (rightIndex < right.length){
+      arrOrdenado.push(right[rightIndex]);
+      rightIndex++
+  }
+  
+  return arrOrdenado;
 }
 
 // No modificar nada debajo de esta línea
